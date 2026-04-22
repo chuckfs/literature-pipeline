1e1aa506cee4c611c5c3544e90303730e5a32ae5
CHUCK <204468377+chuckfs@users.noreply.github.com>
2026-04-22 14:59:39 -0400

    Add JOBS configuration for daily parser

---
+++ config.py
@@ -0,0 +1,11 @@
+JOBS = [
+    {
+        "input": "input/na/just_for_today.pdf",
+        "output": "output/na",
+        "parser": "generic",  # start simple
+        "program": "NA",
+        "book": "Just For Today",
+        "type": "daily",
+        "structure": "entry_list"
+    }
+]
