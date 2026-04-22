33667164f2d4dc57633c7de9605362926157c261
CHUCK <204468377+chuckfs@users.noreply.github.com>
2026-04-22 15:16:12 -0400

    Add new book configurations to config.py
    Added configuration entries for Basic Text and Living Clean.

--- config.py
+++ config.py
@@ -8,4 +8,20 @@ JOBS = [
         "type": "daily",
         "structure": "entry_list"
     }
+    {
+        "input": "input/na/basictext.pdf",
+        "output": "output/na/basic_text",
+        "parser": "na.basic_text",
+        "program": "NA",
+        "book": "Basic Text",
+        "type": "chaptered"
+    }
+    {
+        "input": "input/na/livingclean.pdf",
+        "output": "output/na/living_clean",
+        "parser": "na.living_clean",
+        "program": "NA",
+        "book": "Living Clean",
+        "type": "chaptered"
+    }
 ]
